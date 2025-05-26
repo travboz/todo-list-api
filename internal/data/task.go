a0bdc50acf7ba6a2ca22ab2248b554ebeb18af97
Travis Bozic <travis.bozic@hotmail.com>
2025-05-26 22:38:58 +1000

    brainstorm: figuring out order of db creation

--- internal/data/task.go
+++ internal/data/task.go
@@ -8,7 +8,7 @@ import (
 
 type Task struct {
 	ID          primitive.ObjectID `bson:"_id,omitempty"`
-	Owner       primitive.ObjectID `bson:"owner" json:"owner"`
+	Owner       primitive.ObjectID `bson:"owner" json:"owner"` // use a dummy, so just start on users then create tasks and use the first user you insert's id
 	Title       string             `bson:"title" json:"title"`
 	Description string             `bson:"description" json:"description"`
 	Completed   bool               `bson:"completed" json:"completed"`
